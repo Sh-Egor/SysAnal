9f041bb0a21bcadd6b71d1f428dd1ebaa71d7792
Egor Shpitko <egor.shpitko.00@mail.ru>
2019-06-08 02:03:42 +0400

    hidden-unhidden

--- public/scripts/shops.js
+++ public/scripts/shops.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('logo').addEventListener('click', () => {
-        window.location.href = '/';
+        window.location.href = '/'; //main page
+        var food = document.getElementById('foods');
+        var food1 = document.getElementById('foods1');
+        var kabinet = document.getElementById('kabinet');
+        var kabinet1 = document.getElementById('kabinet1');
+        food.removeAttribute('hidden');
+        food.style.display = "flex";
+        food1.setAttribute('hidden', 'true');
+        food1.style.display = "none";
+        //food - unhidden
+        //food.style display = flex
+        //food1 - hidden
+        //food1.style display = none
+        kabinet1.setAttribute('hidden', 'true');
+        kabinet1.style.display = "none";
+        kabinet.removeAttribute('hidden');
+        kabinet.style.display = "flex";
+        //kabinet1 - hidden
+        //kabinet1.style display - none
+        //kabinet - unhidden
+        //kabinet.style display - flex
     });
     document.getElementById('kabinet').addEventListener('click', () => {
         document.getElementById('signin').removeAttribute('hidden');
@@ -12,13 +32,55 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('foods').style.display = "none";
         document.getElementById('signup').removeAttribute('hidden');
     });
+    document.getElementById('kabinet1').addEventListener('click', () => {
+        var food = document.getElementById('foods');
+        var food1 = document.getElementById('foods1');
+        var kabinet = document.getElementById('kabinet');
+        var kabinet1 = document.getElementById('kabinet1');
+        food.removeAttribute('hidden');
+        food.style.display = "flex";
+        food1.setAttribute('hidden', 'true');
+        food1.style.display = "none";
+        //food - unhidden
+        //food.style display = flex
+        //food1 - hidden
+        //food1.style display = none
+        kabinet1.setAttribute('hidden', 'true');
+        kabinet1.style.display = "none";
+        kabinet.removeAttribute('hidden');
+        kabinet.style.display = "flex";
+        //kabinet1 - hidden
+        //kabinet1.style display - none
+        //kabinet - unhidden
+        //kabinet.style display - flex
+    });
     document.getElementById('foods').addEventListener('click', e => {
         var id = e.target.id;
         $.ajax({
             data: { 'id': id },
             url: '/findUrls',
             success: data => {
-                
+                var food = document.getElementById('foods');
+                var food1 = document.getElementById('foods1');
+                var kabinet = document.getElementById('kabinet');
+                var kabinet1 = document.getElementById('kabinet1');
+                food1.removeAttribute('hidden');
+                food1.style.display = "flex";
+                food.setAttribute('hidden', 'true');
+                food.style.display = "none";
+                //food - unhidden
+                //food.style display = flex
+                //food1 - hidden
+                //food1.style display = none
+                kabinet.setAttribute('hidden', 'true');
+                kabinet.style.display = "none";
+                kabinet1.removeAttribute('hidden');
+                kabinet1.style.display = "flex";
+                //kabinet1 - hidden
+                //kabinet1.style display - none
+                //kabinet - unhidden
+                //kabinet.style display - flex
+                document.getElementById('fastfood').getElementsByTagName('h1')[0].textContent = data.h1;
             }
         });
     });
